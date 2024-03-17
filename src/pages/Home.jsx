c18c7c3c540518fba6a8cce82463268b1ea96f9b
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-03-17 17:08:16 +0100

    Completed Roadmap section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { Benefits } from "../components/Benefits";
 import Collaboration from "../components/Collaboration";
 import Services from "../components/Services";
 import Pricing from "../components/Pricing";
+import Roadmap from "../components/Roadmap";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
